01061bdac87a668477246e59ccc7bb98985d2fd7
Darko <cekovski.darko@gmail.com>
2026-06-07 20:18:11 +0200

    update buttons

--- resources/views/livewire/components/download-cv.blade.php
+++ resources/views/livewire/components/download-cv.blade.php
@@ -1,4 +1,4 @@
-<x-primary-button variant="gradient" size="sm" wire:click="download">
+<x-primary-button variant="amber" size="sm" wire:click="download">
     <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2.5" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round"
               d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"/>
