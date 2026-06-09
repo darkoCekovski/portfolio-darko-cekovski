084fc184eb43bdbafe56edef0db51f45ce90163a
Darko <cekovski.darko@gmail.com>
2026-06-09 16:53:26 +0200

    update Projects, create ProjectSeeder

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,9 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    protected $fillable = ['title', 'description', 'thumbnail', 'demo_url', 'github_url', 'tech_stack'];
+    protected $fillable = [
+        'title',
+        'description',
+        'short_description',
+        'thumbnail',
+        'demo_url',
+        'github_url',
+        'tech_stack',
+        'is_featured',
+        'github_is_public',
+        'order',
+    ];
 
     protected $casts = [
-        'tech_stack' => 'array',
+        'tech_stack'       => 'array',
+        'is_featured'      => 'boolean',
+        'github_is_public' => 'boolean',
     ];
 }
