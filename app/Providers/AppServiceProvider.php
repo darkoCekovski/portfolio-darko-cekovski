3d4ea25f20684c383680749e862944e68149a811
Darko <cekovski.darko@gmail.com>
2026-06-10 12:21:03 +0200

    Add custom SMTP transport to fix SSL CN mismatch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,37 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Mail::extend('smtp', function (array $config) {
+            $transport = new EsmtpTransport(
+                $config['host'],
+                $config['port'] ?? 587,
+                ($config['encryption'] ?? null) === 'ssl',
+            );
+
+            $transport->setUsername($config['username']);
+            $transport->setPassword($config['password']);
+
+            $transport->getStream()->setStreamOptions([
+                'ssl' => [
+                    'verify_peer'       => false,
+                    'verify_peer_name'  => false,
+                    'allow_self_signed' => true,
+                ],
+            ]);
+
+            return $transport;
+        });
     }
 }
