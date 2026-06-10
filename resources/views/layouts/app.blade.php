dec83cc89a2564f0dadb2bab5ff1545b0dc654cd
Darko <cekovski.darko@gmail.com>
2026-06-10 08:56:38 +0200

    update theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}" class="scroll-smooth dark" data-theme="dark">
+<html lang="{{ app()->getLocale() }}" class="scroll-smooth">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,19 +20,14 @@
     {!! ToastMagic::styles() !!}
 
     <script>
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-theme', 'dark');
-        document.documentElement.classList.add('dark');
+        (() => {
+            const t = localStorage.getItem('theme') || 'system';
+            document.documentElement.setAttribute('data-theme', t);
+            const dark = t === 'dark' || (t === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+            if (dark) document.documentElement.classList.add('dark');
+        })();
     </script>
 
-{{--    <script>--}}
-{{--        (() => {--}}
-{{--            const t = localStorage.getItem('theme') || 'system';--}}
-{{--            document.documentElement.setAttribute('data-theme', t);--}}
-{{--            const dark = t === 'dark' || (t === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches);--}}
-{{--            if (dark) document.documentElement.classList.add('dark');--}}
-{{--        })();--}}
-{{--    </script>--}}
 </head>
 
 <body
