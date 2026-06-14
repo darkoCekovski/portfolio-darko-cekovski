c591ec577d900cfe53dc329a83948014d9b5bea0
Darko <cekovski.darko@gmail.com>
2026-06-14 15:03:00 +0200

    update download-cv

--- resources/views/livewire/components/download-cv.blade.php
+++ resources/views/livewire/components/download-cv.blade.php
@@ -1,7 +1,7 @@
 <x-primary-button variant="amber" size="sm" :href="$cvUrl" target="_blank" rel="noopener">
     <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2.5" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"/>
+              d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"/>
     </svg>
     {{ __('messages.cv_button') }}
 </x-primary-button>
