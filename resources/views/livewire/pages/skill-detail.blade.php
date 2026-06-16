ed320910bbd99a38e41295cdbfa65eeff6b66504
Darko <cekovski.darko@gmail.com>
2026-06-16 20:58:42 +0200

    update

--- resources/views/livewire/pages/skill-detail.blade.php
+++ resources/views/livewire/pages/skill-detail.blade.php
@@ -1,10 +1,10 @@
 {{-- Add this snippet just before the closing </div> of the skills-page to auto-open modal when visiting /skill/{slug} directly --}}
-@if($openSlug)
-<script>
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        window.dispatchEvent(new CustomEvent('open-skill-modal', { detail: { slug: '{{ $openSlug }}' } }));
-    }, 200);
-});
-</script>
-@endif
+{{--@if($openSlug)--}}
+{{--<script>--}}
+{{--document.addEventListener('DOMContentLoaded', () => {--}}
+{{--    setTimeout(() => {--}}
+{{--        window.dispatchEvent(new CustomEvent('open-skill-modal', { detail: { slug: '{{ $openSlug }}' } }));--}}
+{{--    }, 200);--}}
+{{--});--}}
+{{--</script>--}}
+{{--@endif--}}
